3cfc9a5f55671103366beea04daa14941aabcff6
Giovanni Pontillo <g.pontillo1@studenti.uniba.it>
2022-12-10 12:24:38 +0100

    Fix client for UDP connection and add client logic

--- Client/src/clientMain.c
+++ Client/src/clientMain.c
@@ -20,25 +20,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define ECHO_MAX 255
 #define BUFFER_SIZE 1024  // Dimensione del buffer
 #define DEFAULT_PORT 5555 // Numero di porta di default
+#define DEFAULT_IP "127.0.0.1" // IP server di default
 
 
-// FUNZIONE PER GESTIONE ERRORI
+// Funzione per gestione errori
 void errorHandler(char *messaggioDiErrore)
 {
 	printf("%s", messaggioDiErrore);
 	system("pause");
 }
 
-// FUNZIONE PER TERMINARE L'USO DI WINSOCK
+// Funzione per terminare l'uso di winsock
 void clearWinSock() {
 	#if defined WIN32
 	WSACleanup();
 	#endif
 }
 
-// FUNZIONE PER CHIUSURA CONNESSIONE
+// Funzione per chiusura connessione
 void closeConnection(int mySocket)
 {
 	closesocket(mySocket);
@@ -60,100 +63,103 @@ int main(void) {
 
 	#endif
 	
-	// CREAZIONE DELLA SOCKET
 	int clientSocket;
-	clientSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	struct sockaddr_in servAddr;
+	struct sockaddr_in fromAddr;
+	unsigned int fromSize;
+	char echoString[ECHO_MAX];
+	char echoBuffer[ECHO_MAX];
+	int echoStringLen;
+	int respStringLen;
 
+	// Input della stringa di echo
+	printf("Inserisci la stringa echo da inviare al server\n");
+	scanf("%s", echoString);
+	if ((echoStringLen = strlen(echoString)) > ECHOMAX)
+		ErrorHandler("echo word too long");
+
+	// Creazione della socket
+	clientSocket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	if (clientSocket < 0) {
 		errorHandler("Errore! Creazione della socket fallita.\n");
 		closeConnection(clientSocket);
 		return -1;
 	}
 
-	// Inserimento da tastiera del numero di porta del server
-	int port;
-	printf("Inserisci il numero di porta del server (%d):", DEFAULT_PORT);
-	scanf("%d", &port);
+	// Costruzione dell'indirizzo del server
+	char* inputForServer = "";
+	int inputIntForServer = 0;
+	char* ipServer = DEFAULT_IP;
+	int portServer = DEFAULT_PORT;
 
-	// COSTRUZIONE DELL’INDIRIZZO DEL SERVER
-	struct sockaddr_in socketAddress;
-	memset(&socketAddress, 0, sizeof(socketAddress));
+	printf("Inserisci IP del server da contattare (default: '%s'): ", DEFAULT_IP);
+	scanf("%s", &ipServer);
+	if(inputForServer == "\n")
+		ipServer = inputForServer;
 
-	socketAddress.sin_family = AF_INET; //famiglia del protocollo
-	socketAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP del server
-	socketAddress.sin_port = htons(port); // Numero di porta del server
+	printf("Inserisci porta del server da contattare (default: '%d'): ", DEFAULT_PORT);
+	scanf("%d", &inputIntForServer);
+	if(inputIntForServer == '\n')
+		portServer = inputIntForServer;
 
-	// CONNESSIONE AL SERVER
-	if (connect(clientSocket, (struct sockaddr *)&socketAddress, sizeof(socketAddress)) < 0)
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = PF_INET;
+	servAddr.sin_port = htons(portServer);
+	servAddr.sin_addr.s_addr = inet_addr(ipServer);
+
+	// Invio della stringa echo al server
+	if (sendto(clientSocket, echoString, echoStringLen, 0, (struct sockaddr*)&servAddr, sizeof(servAddr)) != echoStringLen) {
+		ErrorHandler("sendto() sent different number of bytes than expected");
+		exit(EXIT_FAILURE);
+	}
+
+	// Ritorno della stringa echo
+	fromSize = sizeof(fromAddr);
+	respStringLen = recvfrom(clientSocket, echoBuffer, ECHO_MAX, 0, (struct sockaddr*)&fromAddr, &fromSize);
+
+	if (servAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr)
 	{
-		errorHandler("Errore! Connessione fallita.\n");
-		closeConnection(clientSocket);
-		return -1;
+		fprintf(stderr, "Error: received a packet from unknown source.\n");
+		exit(EXIT_FAILURE);
+	}
+	echoBuffer[respStringLen] = '\0';
+	printf("Received: %s\n", echoBuffer);
+
+	// Lettura stringa su cui contare le vocali
+	char buffer[BUFFER_SIZE] = "";
+	printf("Inserisci stringa su cui leggere le vocali:");
+	scanf("%s", &buffer);
+
+	// Conteggio e invio delle vocali
+	int vowels = 0;
+	int bufferLen = strlen(buffer);
+	for(int i = 0; i < bufferLen; i++) {
+		if(buffer[i] == 'A' || buffer[i] == 'a' || buffer[i] == 'E' || buffer[i] == 'e' || buffer[i] == 'I' || buffer[i] == 'i' || buffer[i] == 'O' || buffer[i] == 'o' || buffer[i] == 'U' || buffer[i] == 'u') {
+			vowels++;
+			if (sendto(clientSocket, buffer, bufferLen, 0, (struct sockaddr*)&servAddr, sizeof(servAddr)) != bufferLen) {
+				ErrorHandler("sendto() sent different number of bytes than expected");
+				closeConnection(clientSocket);
+			}
+		}
 	}
 
-	// GESTIONE DELLA CONNESSIONE COL SERVER
-	// Ricezione della stringa "connnessione avvenuta"
-	char buffer[BUFFER_SIZE]; // buffer per i dati dal server
-	int bytesRicev = 0;
+	// Ricezione delle vocali maiuscole
+	while(vowels > 0) {
+		vowels--;
+		char bufferRecv[BUFFER_SIZE] = "";
+		int respLen;
+		respLen = recvfrom(clientSocket, bufferRecv, ECHO_MAX, 0, (struct sockaddr*)&fromAddr, &fromSize);
 
-	if ((bytesRicev = recv(clientSocket, buffer, BUFFER_SIZE, 0)) <= 0) {
-		errorHandler("Errore! Funzione recv() fallita.\n");
-		closeConnection(clientSocket);
-		return -1;
+		if (servAddr.sin_addr.s_addr != fromAddr.sin_addr.s_addr)
+		{
+			fprintf(stderr, "Error: received a packet from unknown source.\n");
+			exit(EXIT_FAILURE);
+		}
+		bufferRecv[respLen] = '\0';
+		printf("Received: %s\n", bufferRecv);
 	}
-	printf("%s\n", buffer);
 
-	//Ciclo do-while fin quando non riceviamo la stringa bye
-	do {
-		char aString[BUFFER_SIZE] = ""; // Stringa A da inviare
-		char bString[BUFFER_SIZE] = ""; // Stringa B da inviare
-
-
-		// Inserimento stringhe A e B da tastiera
-		printf("Inserisci stringa A:");
-		scanf("%s", aString);
-
-		printf("Inserisci stringa B:");
-		scanf("%s", bString);
-
-		// INVIO STRINGHE AL SERVER
-		// Invio stringa A
-		strcpy(buffer, aString);
-		if (send(clientSocket, buffer, BUFFER_SIZE, 0) <= 0) {
-
-			errorHandler("Errore! Funzione send() fallita.\n");
-			closeConnection(clientSocket);
-			return -1;
-		}
-
-		// Invio stringa B
-		strcpy(buffer, bString);
-		if (send(clientSocket, buffer, BUFFER_SIZE, 0) <= 0) {
-
-			errorHandler("Errore! Funzione send() fallita.\n");
-			closeConnection(clientSocket);
-			return -1;
-		}
-
-		// RICEZIONE STRINGA DAL SERVER
-		int bytesRcvd;
-		printf("Ricevuto: ");
-
-		if ((bytesRcvd = recv(clientSocket, buffer, BUFFER_SIZE, 0)) <= 0) {
-
-			errorHandler("Errore! Funzione recv() fallita.\n");
-			closeConnection(clientSocket);
-			return -1;
-		}
-
-		printf("%s\n", buffer);
-		printf("---------------\n");
-
-	}while(strcmp(buffer,"bye") != 0);
-
-	// CHIUSURA DELLA CONNESSIONE
 	closeConnection(clientSocket);
-	printf("\n");
 	system("pause");
-	return 0;
+	return EXIT_SUCCESS;
 }
